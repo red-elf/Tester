894ae9fbafb2d37ba409fc20d86d5506242bc0e0
Milos Vasic <milos85vasic@gmail.com>
2022-07-27 20:52:25 +0300

    Incorporating the 'Versionable' into the CMake.

--- Recipes/install.sh
+++ Recipes/install.sh
@@ -5,7 +5,8 @@ echo "Installing the 'Tester' micro-framework, please wait" && \
   rm -rf ./Build && \
   mkdir Build && \
   cd Build && \
-  cmake .. && \
+  source ../../Version/version.sh && cmake -DVERSIONABLE_VERSION_PRIMARY="$VERSIONABLE_VERSION_PRIMARY" \
+    -DVERSIONABLE_VERSION_SECONDARY="$VERSIONABLE_VERSION_SECONDARY" .. && \
   make && \
   sudo make install && \
   echo "The 'Tester' micro-framework has been installed with success"
